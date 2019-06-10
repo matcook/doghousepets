08eec0f493f36dcc6900577d25ede399c491f9bf
Mathew Cook <mat.cook@bigpond.com>
2019-06-10 11:42:38 +0800

    Added dependency injection to pet store friends controller and block. updated twig template file to include a button

--- web/modules/custom/pet_store_friends/src/Controller/PostController.php
+++ web/modules/custom/pet_store_friends/src/Controller/PostController.php
@@ -2,13 +2,41 @@
 
 namespace Drupal\pet_store_friends\Controller;
 
-class PostController {
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\pet_store_friends\FriendsBlog;
+
+class PostController extends ControllerBase{
+
+
+  /**
+   *  @var \Drupal\pet_store_friends\FriendsBlog
+  */
+  private $helper;
+  
+    /**
+   * @param \Drupal\pet_store_friends\FriendsBlog $helper
+   */
+  public function __construct(FriendsBlog $helper){
+    $this->helper = $helper;
+  } 
+
   public function getPosts(){
-    $helper = \Drupal::service('pet_store_friends.postHelper');
     return [
       '#theme' => 'post_list',
-      '#posts' => $helper->getPosts(),
+      '#posts' => $this->helper->getPosts(),
       '#title' => 'Friends Pet Blog'
     ];
+  }
+
+    /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container){
+    return new static(
+      $container->get('pet_store_friends.postHelper')
+    );
   }
 }

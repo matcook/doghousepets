6402bce1d4151abba1d189b70d1cd1d14b330bf2
Mathew Cook <mat.cook@bigpond.com>
2019-06-11 15:22:54 +0800

    Added images to custom module output

--- web/modules/custom/pet_store_friends/src/FriendsBlog.php
+++ web/modules/custom/pet_store_friends/src/FriendsBlog.php
@@ -7,25 +7,33 @@ class FriendsBlog {
 
   
   /**
-   * The url to fetch data from. 
+   * The url to fetch post data from. 
    * 
    *  @var string 
   */
-  protected $url = 'https://jsonplaceholder.typicode.com/posts';
+  protected $postUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-  /** 
-   * The JSON data from the API.
+    /**
+   * The url to fetch image data from. 
    * 
-   * @var string 
-   */
-  protected $data;
+   *  @var string 
+  */
+  protected $imgUrl = 'https://jsonplaceholder.typicode.com/photos';
 
   /**
-   * The data from the API. 
+   * The post data from the API. 
    * 
    *  @var array|object 
    */
   protected $posts;
+
+  /**
+   * The image data from the API. 
+   * 
+   *  @var array|object 
+   */
+  protected $photos;
+
 
     /** 
    * @var number
@@ -84,8 +92,13 @@ class FriendsBlog {
    * @return array
    */
   public function getPosts() {
-    $this->data = $this->httpclient->get($this->url)->getBody();
-    $this->posts = json_decode($this->data);
+    $this->posts = json_decode($this->httpclient->get($this->postUrl)->getBody());
+    $this->photos =json_decode($this->httpclient->get($this->imgUrl)->getBody());
+    
+    foreach($this->posts as $post){
+      $post->image_url = $this->photos[$post->id]->url;
+    }
+
     $this->posts = array_slice($this->posts, $this->postID, $this->numberOfPosts);
     return $this->posts;
   }

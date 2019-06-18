6a110f2de9a5a3a342223cf8e32cf7d9b99f5154
Mathew Cook <mat.cook@bigpond.com>
2019-06-18 11:23:07 +0800

    Added dependency injection to block. removed template to use render element.

--- web/modules/custom/map_location_block/js/map.js
+++ web/modules/custom/map_location_block/js/map.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * 
+ *
  * Contains JS functions
  */
 
--- web/modules/custom/map_location_block/src/Plugin/Block/MapLocationBlock.php
+++ web/modules/custom/map_location_block/src/Plugin/Block/MapLocationBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\map_location_block\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * Provides block displaying map location.
@@ -13,21 +16,43 @@ use Drupal\Core\Block\BlockBase;
  *  category = @Translation("Blocks")
  * )
  */
-class MapLocationBlock extends BlockBase {
+class MapLocationBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->config = $config->get('map_location_block.adminsettings');
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $config = \Drupal::config('map_location_block.adminsettings');
     return [
-      '#theme' => 'map_location_block',
+      '#type' => 'inline_template',
+      '#template' => '<div id="map-container"><div id="map" style="height:500px"></div></div>',
       '#attached' => [
         'library' => 'map_location_block/map_block_js',
         'drupalSettings' => [
           'mapLocationBlock' => [
-            'latitude' => $config->get('latitude'),
-            'longitude' => $config->get('longitude'),
+            'latitude' => $this->config->get('latitude'),
+            'longitude' => $this->config->get('longitude'),
           ],
         ],
       ],
